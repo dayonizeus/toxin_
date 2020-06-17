30a3d3550f70a6001c58b7c263d4da96292443b0
Dayonizeus <dayonizeus@gmail.com>
2020-06-17 14:04:33 +0300

    Добавление автоматизации вывода окончания для card-room

---
+++ src/blocks/card-room/card-room.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import caseEnding from '../../js/helperFunctions';
+
+$('.card-room_reviews-quantity').each(function() {
+	var reviewsQuantity = Number($(this).text());
+	var reviewLabel = caseEnding(reviewsQuantity, 'Отзыв', 'Отзыва', 'Отзывов');
+	$(this).next().text(reviewLabel);
+})
